7ced5ce52e8cbf120450040bb9cfe9ba3f5623e1
ocho4 <you@example.com>
2026-04-28 23:24:30 -0500

    Estructura Inicial del backend - Mattias

---
+++ src/main/java/com/upc/co/backend_careme/BackendCareMeApplication.java
@@ -0,0 +1,13 @@
+package com.upc.co.backend_careme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCareMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCareMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/co/backend_careme/BackendCareMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.co.backend_careme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCareMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
